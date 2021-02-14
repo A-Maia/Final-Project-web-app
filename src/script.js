761c88171ddc051376117a3f34dfdb74cef27b09
Ana Fernandes <38138824+A-Maia@users.noreply.github.com>
2021-02-14 23:01:48 +0000

    added variable city

--- src/script.js
+++ src/script.js
@@ -1,38 +1,5 @@
-function showCurrentData(response) {
-  console.log(response.data);
-  let temperatureCurrent = document.querySelector("#temperature");
-  let cityCurrent = document.querySelector("#city");
-  let descriptionCurrent = document.querySelector("#conditions");
-  let humidityCurrent = document.querySelector("#humidity");
-  let windCurrent = document.querySelector("#wind");
-  let maxTempCurrent = document.querySelector("#max-temp");
-  let minTempCurrent = document.querySelector("#min-temp");
-
-  temperatureCurrent.innerHTML = Math.round(response.data.main.temp);
-  cityCurrent.innerHTML = response.data.name;
-  descriptionCurrent.innerHTML = response.data.weather[0].description;
-  humidityCurrent.innerHTML = `${response.data.main.humidity}%`;
-  windCurrent.innerHTML = `${Math.round(response.data.wind.speed)}km/h`;
-  maxTempCurrent.innerHTML = `↑${Math.round(response.data.main.temp_max)}°C`;
-  minTempCurrent.innerHTML = `↓${Math.round(response.data.main.temp_min)}°C`;
-}
-
-let apiKey = "3cd7c0aa89391f850a62418573a9be62";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
-
-axios.get(apiUrl).then(showCurrentData);
-
+// current date, month, year
 let now = new Date();
-
-//let timeHour = now.getHours();
-//if (timeHour < 10) {
-//timeHour = `0${timehour}`;
-//}
-//let timeMinutes = now.getMinutes();
-//if (timeMinutes < 10) {
-//timeMinutes = `0${timeMinutes}`;
-//}
-
 let days = [
   `Sunday`,
   `Monday`,
@@ -63,3 +30,29 @@ let year = now.getFullYear();
 
 let dateCurrent = document.querySelector("#date");
 dateCurrent.innerHTML = `${weekday}, ${month} ${year}`;
+
+// Current data - temperature related
+
+function showCurrentData(response) {
+  let temperatureCurrent = document.querySelector("#temperature");
+  let cityCurrent = document.querySelector("#city");
+  let descriptionCurrent = document.querySelector("#conditions");
+  let humidityCurrent = document.querySelector("#humidity");
+  let windCurrent = document.querySelector("#wind");
+  let maxTempCurrent = document.querySelector("#max-temp");
+  let minTempCurrent = document.querySelector("#min-temp");
+
+  temperatureCurrent.innerHTML = Math.round(response.data.main.temp);
+  cityCurrent.innerHTML = response.data.name;
+  descriptionCurrent.innerHTML = response.data.weather[0].description;
+  humidityCurrent.innerHTML = `${response.data.main.humidity}%`;
+  windCurrent.innerHTML = `${Math.round(response.data.wind.speed)}km/h`;
+  maxTempCurrent.innerHTML = `↑${Math.round(response.data.main.temp_max)}°C`;
+  minTempCurrent.innerHTML = `↓${Math.round(response.data.main.temp_min)}°C`;
+}
+
+let apiKey = "3cd7c0aa89391f850a62418573a9be62";
+let city = "Paris";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(showCurrentData);
